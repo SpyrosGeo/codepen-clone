c2c258e497e71ca5cb189cbf098e9913423e2977
SpyrosGeo <xarodasimba@gmail.com>
2020-09-20 14:50:38 +0300

    fixed overflow scroll bug added fontawesome icon for button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,37 +4,44 @@ import 'codemirror/theme/material-palenight.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
 export default function Editor(props) {
     const { language, displayName, value, onChange } = props;
-    const [open,setOpen]=useState(true)
+    const [open, setOpen] = useState(true)
 
     const handleChange = (editor, data, value) => {
         onChange(value)
     }
-    const handleClick = ()=>{
-        setOpen(prevOpen =>!prevOpen)
+    const handleClick = () => {
+        setOpen(prevOpen => !prevOpen)
     }
     return (
-        <div className={`editor-container ${open? "":'collapsed'}`}>
+        <div className={`editor-container ${open ? "" : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button onClick={handleClick}>O/C</button>
-                </div>
-                <ControlledEditor
-                    onBeforeChange={handleChange}
-                    value={value}
-                    className="code-mirror-wrapper"
-                    options={{
-                        lineNumbers: true,
-                        lineWrapping: true,
-                        lint: true,
-                        mode: language,
-                        theme:'material-palenight',
-                        fixedGutter:true
-                    }}
-                />
+                <button
+                    type="button"
+                    className="expand-collapse-button"
+                    onClick={handleClick}>
+                    <FontAwesomeIcon
+                        icon={open ? faCompressAlt : faExpandAlt}
+                    /></button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: 'material-palenight',
+                    fixedGutter: true
+                }}
+            />
         </div>
     )
 }
